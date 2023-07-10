d452328a3720524846741a00550f251cf2b1c915
mtmattqq <124585214+mtmattqq@users.noreply.github.com>
2023-07-10 08:50:17 +0800

    vector

---
+++ AppData/vec2D.py
@@ -0,0 +1,6 @@
+import math
+
+class vec2D :
+    def __init__(self) :
+        x = 0
+        y = 0
--- main.py
+++ main.py
@@ -9,16 +9,28 @@ flags = FULLSCREEN | DOUBLEBUF
 resolution = (1980, 1080)
 screen = pygame.display.set_mode(resolution, flags, 16)
 screen.fill((255, 255, 255))
+clock=pygame.time.Clock()
 pygame.display.set_caption("Basic TD Game")
 font=pygame.font.SysFont('microsoftjhenghei',50)
 
 # variables
 FPS=60
+relative_pos = vec2D(0, 0)
+
+def show_text(text = '', x = 0, y = 0, color = (0, 0, 0)) :
+    text=font.render(text, True, color)
+    textRect=text.get_rect()
+    textRect.topleft=(x+relative_pos.x-10, y+relative_pos.y-20)
+    screen.blit(text, textRect)
 
 InGame=True
-while InGame:
+while InGame :
     # event in pygame
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            InGame=False            
+    for event in pygame.event.get() :
+        if event.type == pygame.QUIT :
+            InGame = False
+        if event.type == pygame.KEYDOWN :
+            if event.unicode == 'q' :
+                InGame = False
+    clock.tick(FPS)
 pygame.quit()
