8f25dbefc938528c8b0368b171b1839acc0eb7be
mtmattqq <124585214+mtmattqq@users.noreply.github.com>
2023-07-10 09:03:15 +0800

    update

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ screen.fill((255, 255, 255))
 clock=pygame.time.Clock()
 pygame.display.set_caption("Basic TD Game")
 font=pygame.font.SysFont('microsoftjhenghei',50)
+pygame.event.set_allowed([QUIT, KEYDOWN, KEYUP, MOUSEBUTTONDOWN, MOUSEBUTTONUP, MOUSEMOTION, MOUSEWHEEL])
 
 # variables
 FPS=60
